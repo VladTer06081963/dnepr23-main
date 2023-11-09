2623806919fcd3e159032fbdb0b8efa968b3e8e7
VladTer06081963 <patrol200611@ukr.net>
2023-11-09 13:15:51 +0200

    clean

--- index.js
+++ index.js
@@ -12,6 +12,31 @@ function closeModal() {
   modal.style.display = "none";
 }
 
+// Переключить изображение в модальном окне
+function switchImage(offset) {
+  const modalImage = document.getElementById("modalImage");
+  const currentImageId = getCurrentImageId();
+  const currentIndex = parseInt(currentImageId.slice(-1));
+
+  const newIndex = currentIndex + offset;
+  const newImageId = "image" + newIndex;
+
+  console.log("Trying to switch to:", newImageId); // Добавьте эту строку для отладки
+
+  // Assuming you have a total of 6 images
+  if (newIndex >= 1 && newIndex <= 6) {
+    modalImage.src = "img/" + newImageId + ".webp";
+  }
+}
+
+// Получить текущий идентификатор изображения в модальном окне
+
+function getCurrentImageId() {
+  const modalImageSrc = document.getElementById("modalImage").src;
+  const matches = modalImageSrc.match(/image(\d+)/);
+  return matches ? matches[0] : null;
+}
+
 // Закрыть модальное окно, если пользователь щелкает вне изображения
 window.onclick = function (event) {
   const modal = document.getElementById("myModal");
